3e9da3569b2ad344d21b02cbfcde17e4b6de3857
William Held <helw150@aa.edu>
2015-04-14 10:06:12 -0600

    comments

--- gaussian.m
+++ gaussian.m
@@ -1,11 +1,13 @@
 function x = Gauss(a,b)
 ab = [a,b];
 [M, N] = size(ab);
-for j = 1:M-1
+% forward subsitution
+for j = 1:M-1       % loops across the columns
     for i = j+1:M
         ab(i,j:N) = ab(i,j:N)-ab(i,j)/ab(j,j)*ab(j,j:N)
     end
 end
+% backwards substitution
 x = zeros(M,1);
 x(M) = ab(M,N)/ab(M,M);
 for i = R-1:-1:1
