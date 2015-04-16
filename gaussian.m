ede72d12e9e2289e2226f972423052f4cfd8d032
William Held <helw150@aa.edu>
2015-04-16 11:52:55 -0600

    Error fixes.
    Working version of GEPP

--- gaussian.m
+++ gaussian.m
@@ -1,6 +1,6 @@
 function x = Gauss(a,b)
 
-[a,b] = partialp(a,b)
+ab = partialp(a,b)
 [M, N] = size(ab);
 % forward elimination
 for j = 1:M-1       % loops across the columns
@@ -11,6 +11,6 @@ end
 % backwards substitution
 x = zeros(M,1);
 x(M) = ab(M,N)/ab(M,M);
-for i = R-1:-1:1
-    x(i)=(ab(i,N)-ab(i,i+1:R))/ab(i,i);
+for i = M-1:-1:1
+    x(i)=(ab(i,N)-ab(i,i+1:M)*x(i+1:M))/ab(i,i);
 end
--- partialp.m
+++ partialp.m
@@ -1,12 +1,12 @@
 
-function [ab] =  partialp(a,b)
+function ab =  partialp(a,b)
 ab = [a,b]
-[M, N] = size(ab)
-for j = 1:n-1
+[M,N] = size(ab)
+for j = 1:M-1
     p=j;
-    maxim=abs(a(j,j); % the first entry 
+    maxim=abs(a(j,j)); % the first entry 
 
-    for i = j+1:N
+    for i = j+1:M
         tmp=abs(a(i,j)); %create a placeholder variable for the next location
         if tmp > maxim
            maxim=tmp;
