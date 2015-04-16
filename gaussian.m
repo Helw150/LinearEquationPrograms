d9fc899e95acfcaa283ee15b28e428ce969af632
William Held <helw150@aa.edu>
2015-04-16 13:57:52 -0600

    Partial Pivoting correction
    Makes partial pivoting occur after every forward elimination.

--- gaussian.m
+++ gaussian.m
@@ -1,14 +1,15 @@
 function x = Gauss(a,b)
-
-ab = partialp(a,b)
-[M, N] = size(ab);
+ab = [a,b] 
+[M, N] = size(ab); 
+for j = 1:M-1 
+   ab = partialp(ab,j,M)
 % forward elimination
-for j = 1:M-1       % loops across the columns
+
     for i = j+1:M
         ab(i,j:N) = ab(i,j:N)-ab(i,j)/ab(j,j)*ab(j,j:N)
     end
 end
-% backwards substitution
+% bacjwards substitution
 x = zeros(M,1);
 x(M) = ab(M,N)/ab(M,M);
 for i = M-1:-1:1
--- partialp.m
+++ partialp.m
@@ -1,20 +1,15 @@
 
-function ab =  partialp(a,b)
-ab = [a,b]
-[M,N] = size(ab)
-for j = 1:M-1
-    p=j;
-    maxim=abs(a(j,j)); % the first entry 
+function [ab] =  partialp(ab,j,M)
+p=j;
+maxim=abs(ab(j,j)); % the first entry 
+for i = j+1:M
+    tmp=abs(ab(i,j)); %create a placeholder variable for the next location
+    if tmp > maxim
+       maxim=tmp;
+       p=i;
+    end
+end 
 
-    for i = j+1:M
-        tmp=abs(a(i,j)); %create a placeholder variable for the next location
-        if tmp > maxim
-           maxim=tmp;
-           p=i;
-        end
-    end 
-
-    tmp=ab(p,:);     %perform the row swap
-    ab(p,:) = ab(j,:);  
-    ab(j,:) = tmp;   
-end
+tmp=ab(p,:);     %perform the row swap
+ab(p,:) = ab(j,:);  
+ab(j,:) = tmp;   
