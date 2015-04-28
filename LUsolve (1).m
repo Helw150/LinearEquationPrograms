927ee34d18c2aed3adac16f440aeb6fd3adc1fc8
William Held <helw150@aa.edu>
2015-04-28 13:07:07 -0600

    LU solve with front and back sub

---
+++ LUsolve (1).m
@@ -0,0 +1,20 @@
+function x = LUsolve(A,B)
+[L, U, P] = LUdecomp(A);
+N = length(A)
+x = zeros(N,1);
+y = zeros(N,1);
+
+for i = 1:N
+    tmp = b(i);
+    for j = 1:i-1
+        tmp = tmp - L(i,j)*y(j)
+    end
+    y(i) = tmp
+end
+for i = N:-1:1
+    tmp = y(i)
+    for j = i+1:N
+        tmp = tmp - U(i,j)*x(j);
+    end
+    x(i) = tmp / U(i,i);
+end
