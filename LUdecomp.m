c23efdbb91aece71bd7c6972fbdf03bb48bdc342
William Held <helw150@aa.edu>
2015-04-30 14:00:17 -0600

    Memory Efficient LU Decomposition
    No longer requires storage and permutation of A

--- LUdecomp.m
+++ LUdecomp.m
@@ -1,38 +1,35 @@
 function[L, U, P] = LUdecomp(A)
 N = length(A);
-P = eye(N);
+P = [1:N];
 U = zeros(N);
 L = zeros(N);
 for j = 1:N
     p = j;
-    maxim = abs(A(j,j)); % the first entry 
+    maxim = abs(A(P(j),j)); % the first entry 
     for i = j+1:N
-        tmp = abs(A(i,j));
+        tmp = abs(A(P(i),j));
         if tmp > maxim
            maxim = tmp;
            p = i;
         end
     end
 
-    tmp = A(p,:);     %perform the row swap on U
-    A(p,:) = A(j,:);  
-    A(j,:) = tmp;
-    tmp = P(p,:);     %perform the row swap on P
-    P(p,:) = P(j,:);  
-    P(j,:) = tmp;
+    tmp = P(p);     %perform the row swap on P
+    P(p) = P(j);  
+    P(j) = tmp;
    
     for i = 1:j 
-           L(i,1) = A(i,1); 
+           L(i,1) = A(P(i),1); 
 	   U(i,i) = 1;
     end
     for i = 2:j
-	U(1,i)= A(1,i)/L(1,1);
+	U(1,i)= A(P(1),i)/L(1,1);
     end
     for k = 2:j 
-        L(j,k)=A(j,k)-L(j,1:k-1)*U(1:k-1,k); 
+        L(j,k)=A(P(j),k)-L(j,1:k-1)*U(1:k-1,k); 
     end 
     for k = j+1:N
-        U(j,k)=(A(j,k)-L(j,1:j-1)*U(1:j-1,k))/L(j,j); 
+        U(j,k)=(A(P(j),k)-L(j,1:j-1)*U(1:j-1,k))/L(j,j); 
     end
 end
 return;
